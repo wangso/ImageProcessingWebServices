f8cc153d3299b234e522357b754645e338639576
Songjie Wang <wangso@missouri.edu>
2019-06-07 11:01:43 -0500

    created result.txt file for FPS collection

--- client.py
+++ client.py
@@ -41,7 +41,7 @@ while True:
 	currentFPS = 1.0/(time.time() - frameStartTime)
 	FPS.append(currentFPS)
 	print("response = {}, frame = {}, fps = {} ".format(r, frameCount, round(currentFPS, 3)))
-        file2 = open("output.txt", "a")
+        file2 = open("result.txt", "a")
         file2.write("response = {}, frame = {}, fps = {} ".format(r, frameCount, round(currentFPS, 3)))
         file2.close
 	if r == "<Response [500]>":
